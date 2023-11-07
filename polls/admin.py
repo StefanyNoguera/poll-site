1230bbb6fdb9c80e29281b8243df8ada64485405
Stefany Noguera <stefanynoguerar@gmail.com>
2023-11-07 17:20:58 +0000

    admin form changed

--- polls/admin.py
+++ polls/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from .models import Question
 
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
 
-# Register your models here.
+
+admin.site.register(Question, QuestionAdmin)
